ed6e052c01c2af30f30f194c3dbeea4cf1f92e81
indrani-gthb1 <indranijiko@gmail.com>
2025-07-16 20:36:18 +0530

    Update stlt1.py

--- stlt1.py
+++ stlt1.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title("hello i am workinng in streamlit")
-name-st.text_input("Enter your name")
-st.write("hello...")
+name=st.text_input("Enter your name")
+if st,button=("submit"):
+  st.write("hello...,{name} welcome")
